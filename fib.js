09470f6cec7bebdd959b8b70995653d4ec39f22f
sathiyarajg215 <sathiyarajg215@gmail.com>
2026-04-21 11:39:03 +0530

    sathya

--- fib.js
+++ fib.js
@@ -1,14 +1,4 @@
-// Check Even Number in JavaScript
-
-let num = 10;
-
-if (num % 2 === 0) {
-    console.log("Even Number");
-} else {
-    console.log("Odd Number");
-}
-
-  
+ 
 // fibnocci
 
 function fibonacci(n) {
